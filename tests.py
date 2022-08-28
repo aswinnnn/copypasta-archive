7acaaac2d35fe9c64756f9a21beee4462b669f52
Aswin S <aswinsnair028@gmail.com>
2022-08-28 18:38:58 +0530

    initial tests

---
+++ tests.py
@@ -0,0 +1,3 @@
+import datetime
+
+print(datetime.datetime.now().strftime('%d_%m_%Y.txt'))
---
+++ worker.py
@@ -0,0 +1,18 @@
+from json import loads
+import urllib3
+from datetime import datetime
+from pathlib import Path 
+
+file = datetime.now().strftime('%d_%m_%Y.txt')
+
+http = urllib3.PoolManager()
+url = "https://www.reddit.com/r/copypasta/top.json"
+content = http.request('GET', url)
+content = loads(content.data.decode('utf-8'))
+
+pasta = content['data']['children'][0]['data']['selftext']
+title = content['data']['children'][0]['data']['title']
+
+with open(Path(rf".\pasta\{file}"), "x", encoding='utf-8') as f:
+    f.write(f"{title}\n\n{pasta}")
+
